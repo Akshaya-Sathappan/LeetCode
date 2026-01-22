f2fea903c12e875d7453e3711c3b30a0311737b2
Akshaya-Sathappan <121086140+Akshaya-Sathappan@users.noreply.github.com>
2026-01-22 14:12:50 -0800

    0121-best-time-to-buy-and-sell-stock

---
+++ LeetCode/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int mini = prices[0];
+        for(int i = 1; i < prices.length; i++){
+            maxProfit = Math.max(prices[i] - mini, maxProfit);
+            mini = Math.min(mini, prices[i]);
+        }
+        return maxProfit;
+    }
+}
