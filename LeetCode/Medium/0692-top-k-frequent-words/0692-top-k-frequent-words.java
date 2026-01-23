272c53c452ca43796be86c705503d6e4b9a1fea7
Akshaya-Sathappan <121086140+Akshaya-Sathappan@users.noreply.github.com>
2026-01-23 12:23:18 -0800

    0692-top-k-frequent-words

---
+++ LeetCode/Medium/0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        HashMap<String, Integer> map = new HashMap<>();
+        for(int i = 0; i < words.length; i++){
+            map.put(words[i], map.getOrDefault(words[i], 0) + 1);
+        }
+
+        PriorityQueue<String> pq = new PriorityQueue<>((a, b) -> map.get(a).equals(map.get(b)) ? b.compareTo(a) : (map.get(a) - map.get(b)));
+
+        for(String str : map.keySet()){
+            pq.add(str);
+            if(pq.size() > k){
+                pq.poll();
+            }
+        }
+
+        List<String> list = new ArrayList<>();
+
+        while(!pq.isEmpty()){
+            list.add(pq.poll());
+        }
+
+        Collections.reverse(list);
+
+        return list;
+    }
+}
