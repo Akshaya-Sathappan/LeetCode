f21d17d86072f930972a806105659b79f50be1fc
Akshaya-Sathappan <121086140+Akshaya-Sathappan@users.noreply.github.com>
2026-01-28 13:50:04 -0800

    0073-set-matrix-zeroes

---
+++ LeetCode/Medium/0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -0,0 +1,37 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int col0 = 1;
+
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[0].length; j++){
+                if(matrix[i][j] == 0 && j != 0){
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+                else if(matrix[i][j]== 0 && j == 0){
+                    col0 = 0;
+                }
+            }
+        }
+
+        for(int i = matrix.length - 1; i >= 1; i--){
+            for(int j = matrix[0].length - 1; j >= 1; j--){
+                if(matrix[0][j] == 0 || matrix[i][0] == 0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if(matrix[0][0] == 0){
+            for(int j = 0; j < matrix[0].length; j++){
+                matrix[0][j] = 0;
+            }
+        }
+
+        if(col0 == 0){
+            for(int i = 0; i < matrix.length; i++){
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
