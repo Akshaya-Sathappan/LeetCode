cad346516dc74cf893e5923e0fc3893db45352a6
Akshaya-Sathappan <121086140+Akshaya-Sathappan@users.noreply.github.com>
2026-01-29 13:08:49 -0800

    0430-flatten-a-multilevel-doubly-linked-list

---
+++ LeetCode/Medium/0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.java
@@ -0,0 +1,39 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+};
+*/
+
+class Solution {
+    public Node flatten(Node head) {
+        if(head == null){
+            return null;
+        }
+
+        Stack<Node> stack = new Stack<>();
+        Node curr = head;
+
+        while(curr != null){
+            if(curr.child != null){
+                if(curr.next != null){
+                    stack.push(curr.next);
+                }
+                curr.next = curr.child;
+                curr.child.prev = curr;
+                curr.child = null;
+            }
+            if(curr.next == null && !stack.isEmpty()){
+                Node n = stack.pop();
+                curr.next = n;
+                n.prev = curr;
+            }
+
+            curr = curr.next;
+        }
+        return head;
+    }
+}
