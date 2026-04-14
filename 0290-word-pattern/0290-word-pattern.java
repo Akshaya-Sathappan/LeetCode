aac64ddc67d778e3a52c45a68b97ad09af0007b4
Akshaya-Sathappan <121086140+Akshaya-Sathappan@users.noreply.github.com>
2026-04-13 20:24:23 -0700

    Time: 1 ms (83.03%), Space: 43.1 MB (9.19%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,25 +1,25 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
-        HashMap map_index = new HashMap();
+        HashMap map = new HashMap();
         String[] words = s.split(" ");
 
-        if (words.length != pattern.length())
+        if(words.length != pattern.length()){
             return false;
-
-        for (Integer i = 0; i < words.length; i++) {
-            char c = pattern.charAt(i);
-            String w = words[i];
-
-            if (!map_index.containsKey(c))
-                map_index.put(c, i);
-
-            if (!map_index.containsKey(w))
-                map_index.put(w, i);
-
-            if (map_index.get(c) != map_index.get(w))
-                return false;
         }
 
+        for(Integer i = 0; i < words.length; i++){
+            if(!map.containsKey(words[i])){
+                map.put(words[i], i);
+            }
+
+            if(!map.containsKey(pattern.charAt(i))){
+                map.put(pattern.charAt(i), i);
+            }
+
+            if(map.get(words[i]) != map.get(pattern.charAt(i))){
+                return false;
+            }
+        }
         return true;
     }
 }
