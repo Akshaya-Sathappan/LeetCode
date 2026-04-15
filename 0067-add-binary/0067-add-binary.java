59d5b13d0389ec6df872467b94e86cad266610f2
Akshaya-Sathappan <121086140+Akshaya-Sathappan@users.noreply.github.com>
2026-04-14 18:28:19 -0700

    Time: 2 ms (50.97%), Space: 43.4 MB (91.75%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -6,17 +6,14 @@ class Solution {
         int carry = 0;
 
         while(i >= 0 || j >= 0 || carry != 0){
-            int digit1 = (i >= 0) ? a.charAt(i) - '0' : 0;
-            int digit2 = (j >= 0) ? b.charAt(j) - '0' : 0;
+            int digit1 = (i >= 0) ? a.charAt(i--) - '0' : 0;
+            int digit2 = (j >= 0) ? b.charAt(j--) - '0' : 0;
 
             int sum = digit1 + digit2 + carry;
 
             result.append(sum % 2);
 
             carry = sum / 2;
-
-            i--;
-            j--;
         }
         return result.reverse().toString();
     }
