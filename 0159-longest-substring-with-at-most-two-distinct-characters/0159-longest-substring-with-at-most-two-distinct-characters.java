8debf20ad116dac969014564500c8fc9447ebb3b
Akshaya-Sathappan <121086140+Akshaya-Sathappan@users.noreply.github.com>
2026-04-27 19:39:21 -0700

    Time: 44 ms (35.35%), Space: 47.5 MB (17.76%) - LeetHub

--- 0159-longest-substring-with-at-most-two-distinct-characters/0159-longest-substring-with-at-most-two-distinct-characters.java
+++ 0159-longest-substring-with-at-most-two-distinct-characters/0159-longest-substring-with-at-most-two-distinct-characters.java
@@ -4,10 +4,6 @@ class Solution {
         int left = 0;
         int maxLength = 0;
 
-        if(s.length() < 3){
-            return s.length();
-        }
-
         for(int right = 0; right < s.length(); right++){
             char r = s.charAt(right);
             map.put(r, map.getOrDefault(r, 0) + 1);
@@ -18,8 +14,10 @@ class Solution {
                     map.remove(l);
                 left++;        
             }
+
             maxLength = Math.max(maxLength, right - left + 1);
         }
+        
         return maxLength;
     }
 }
