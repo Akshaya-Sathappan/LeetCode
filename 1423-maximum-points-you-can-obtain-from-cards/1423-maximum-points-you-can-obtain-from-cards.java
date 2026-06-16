1c5c620d39455f5f34b5f25931786c3b3fc3f45c
Akshaya-Sathappan <121086140+Akshaya-Sathappan@users.noreply.github.com>
2026-06-16 00:44:33 -0700

    Time: 5 ms (3.58%), Space: 61.8 MB (52.26%) - LeetHub

---
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int maxScore(int[] cardPoints, int k) {
+        int sum = 0;
+        for(int points : cardPoints){
+            sum += points;
+        }
+
+        return sum - minSumContiguousSubArray(cardPoints, cardPoints.length - k);
+    }
+
+    private int minSumContiguousSubArray(int[] cardPoints, int k){
+        int left = 0;
+        int minSum = 0;
+        int ans = Integer.MAX_VALUE;
+
+        for(int right = 0; right < cardPoints.length; right++){
+            minSum += cardPoints[right];
+
+            if(right - left + 1 > k){
+                minSum -= cardPoints[left];
+                left++;
+            }
+
+            if(right - left + 1 == k){
+                ans = Math.min(ans, minSum);
+            }
+        }
+        return ans;
+    }
+}
