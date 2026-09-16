36cc443896896e62a85cf355806ce06423beda44
Akshaya-Sathappan <121086140+Akshaya-Sathappan@users.noreply.github.com>
2026-09-15 22:46:22 -0700

    Time: 5 ms (97.61%), Space: 52 MB (9.56%) - LeetHub

--- 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.java
+++ 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.java
@@ -1,23 +1,25 @@
 class Solution {
     public int maxTurbulenceSize(int[] arr) {
-        int sign = -1;
         int count = 0;
+        int sign = -1;
         int maxSize = 0;
 
-        for(int i = 0; i < arr.length - 1; i++){
-            if(arr[i] < arr[i + 1]){
-                count = (sign == 1) ? count + 1 : 1;
-                sign = 0;
-            }
-            else if(arr[i] > arr[i + 1]){
+        for(int i = 1; i < arr.length; i++){
+            if(arr[i] < arr[i - 1]){
                 count = (sign == 0) ? count + 1 : 1;
                 sign = 1;
+            }
+            else if(arr[i] > arr[i - 1]){
+                count = (sign == 1) ? count + 1 : 1;
+                sign = 0;
             }
             else{
                 sign = -1;
             }
-            maxSize = Math.max(count, maxSize);
+
+            maxSize = Math.max(maxSize, count);
         }
+
         return maxSize + 1;
     }
 }
