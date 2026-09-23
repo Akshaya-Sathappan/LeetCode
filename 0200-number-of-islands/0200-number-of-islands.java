fb0e76228baf43424dc1d736443e87cf03d4063c
Akshaya-Sathappan <121086140+Akshaya-Sathappan@users.noreply.github.com>
2026-09-23 00:05:20 -0700

    Time: 83 ms (5.27%), Space: 52.5 MB (21.46%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,8 +1,8 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int count = 0;
         int nRows = grid.length;
         int nColumns = grid[0].length;
+        int count = 0;
 
         for(int i = 0; i < nRows; i++){
             for(int j = 0; j < nColumns; j++){
@@ -16,17 +16,18 @@ class Solution {
         return count;
     }
 
-    public void helper(char[][] grid, int r, int c){
-
-        if(r < 0 || c < 0 || r >= grid.length || c >= grid[0].length || grid[r][c] == '0'){
+    public void helper(char[][] grid, int i, int j){
+        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0'){
             return;
         }
 
-        grid[r][c] = '0';
+        grid[i][j] = '0';
 
-        helper(grid, r + 1, c);
-        helper(grid, r - 1, c);
-        helper(grid, r, c + 1);
-        helper(grid, r, c - 1);
+        helper(grid, i + 1, j);
+        helper(grid, i - 1, j);
+        helper(grid, i, j + 1);
+        helper(grid, i, j - 1);
+
+        return;
     }
 }
